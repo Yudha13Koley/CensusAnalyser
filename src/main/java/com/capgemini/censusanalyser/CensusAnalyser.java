7869fb4e513ddd4fb39f5ea9960435e600dda202
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-18 12:12:29 +0530

    Refactored : To Follow Dry Principle Created a Generic method to get the list from csv file

--- src/main/java/com/capgemini/censusanalyser/CensusAnalyser.java
+++ src/main/java/com/capgemini/censusanalyser/CensusAnalyser.java
@@ -7,7 +7,6 @@ import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 import com.capgemini.exception.CSVBuilderException;
 import com.capgemini.exceptions.CensusAnalyserException;
@@ -18,12 +17,12 @@ import com.capgemini.statecensusanalyser.StateCodeCSV;
 import com.google.gson.Gson;
 
 public class CensusAnalyser {
-	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+
+	private <E> List<E> getListOfData(String csvFilePath, Class<E> objectclass) throws CensusAnalyserException {
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
 			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<IndiaCensusCSV> listOfEntries = csvBuilder.getListForCSVFile(reader, IndiaCensusCSV.class);
-			return listOfEntries.size();
+			return csvBuilder.getListForCSVFile(reader, objectclass);
 		} catch (CSVBuilderException e) {
 			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
 		} catch (NoSuchFileException e) {
@@ -33,131 +32,66 @@ public class CensusAnalyser {
 		} catch (RuntimeException e) {
 			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
 		}
+
+	}
+
+	public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+		List<IndiaCensusCSV> listOfEntries = this.getListOfData(csvFilePath, IndiaCensusCSV.class);
+		return listOfEntries.size();
 	}
 
 	public int loadStateCode(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<StateCodeCSV> listOfStateEntries = csvBuilder.getListForCSVFile(reader, StateCodeCSV.class);
-			return listOfStateEntries.size();
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<StateCodeCSV> listOfEntries = this.getListOfData(csvFilePath, StateCodeCSV.class);
+		return listOfEntries.size();
 	}
 
-	private <E> int getCountOfEntries(Iterator<E> iterator) {
-		Iterable<E> csvIterable = () -> iterator;
-		return (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
-	}
+	/*
+	 * private <E> int getCountOfEntries(Iterator<E> iterator) { Iterable<E>
+	 * csvIterable = () -> iterator; return (int)
+	 * StreamSupport.stream(csvIterable.spliterator(), false).count(); }
+	 */
 
 	public String getSortedlistByState(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<IndiaCensusCSV> listOfEntries = csvBuilder.getListForCSVFile(reader, IndiaCensusCSV.class);
-			Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.state);
-			List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
-			String json = new Gson().toJson(sortedListOfEntries);
-			return json;
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<IndiaCensusCSV> listOfEntries = this.getListOfData(csvFilePath, IndiaCensusCSV.class);
+		Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.state);
+		List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
+		String json = new Gson().toJson(sortedListOfEntries);
+		return json;
 	}
 
 	public String getSortedlistByStateCode(String stateFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(stateFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<StateCodeCSV> listOfStateEntries = csvBuilder.getListForCSVFile(reader, StateCodeCSV.class);
-			Comparator<StateCodeCSV> comparator = Comparator.comparing(stateCensus -> stateCensus.stateCode);
-			List<StateCodeCSV> sortedListOfEntries = this.sortList(listOfStateEntries, comparator);
-			String json = new Gson().toJson(sortedListOfEntries);
-			return json;
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<StateCodeCSV> listOfStateEntries = this.getListOfData(stateFilePath, StateCodeCSV.class);
+		Comparator<StateCodeCSV> comparator = Comparator.comparing(stateCensus -> stateCensus.stateCode);
+		List<StateCodeCSV> sortedListOfEntries = this.sortList(listOfStateEntries, comparator);
+		String json = new Gson().toJson(sortedListOfEntries);
+		return json;
 	}
 
 	public String getSortedlistByPopulation(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<IndiaCensusCSV> listOfEntries = csvBuilder.getListForCSVFile(reader, IndiaCensusCSV.class);
-			Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.population);
-			List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
-			Collections.reverse(sortedListOfEntries);
-			String json = new Gson().toJson(sortedListOfEntries);
-			return json;
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<IndiaCensusCSV> listOfEntries = this.getListOfData(csvFilePath, IndiaCensusCSV.class);
+		Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.population);
+		List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
+		Collections.reverse(sortedListOfEntries);
+		String json = new Gson().toJson(sortedListOfEntries);
+		return json;
 	}
 
 	public String getSortedlistByPopulationDensity(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<IndiaCensusCSV> listOfEntries = csvBuilder.getListForCSVFile(reader, IndiaCensusCSV.class);
-			Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.densityPerSqKm);
-			List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
-			Collections.reverse(sortedListOfEntries);
-			String json = new Gson().toJson(sortedListOfEntries);
-			return json;
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<IndiaCensusCSV> listOfEntries = this.getListOfData(csvFilePath, IndiaCensusCSV.class);
+		Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.densityPerSqKm);
+		List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
+		Collections.reverse(sortedListOfEntries);
+		String json = new Gson().toJson(sortedListOfEntries);
+		return json;
 	}
 
 	public String getSortedlistByStateArea(String csvFilePath) throws CensusAnalyserException {
-		try {
-			Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
-			ICSVBuilder csvBuilder = CSVBuilderFactory.getCSVBuilder();
-			List<IndiaCensusCSV> listOfEntries = csvBuilder.getListForCSVFile(reader, IndiaCensusCSV.class);
-			Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.areaInSqKm);
-			List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
-			Collections.reverse(sortedListOfEntries);
-			String json = new Gson().toJson(sortedListOfEntries);
-			return json;
-		} catch (CSVBuilderException e) {
-			throw new CensusAnalyserException("Unable to Parse", CensusAnalyserException.ExceptionType.PARSE_EXCEPTION);
-		} catch (NoSuchFileException e) {
-			throw new CensusAnalyserException("No Such File Found", CensusAnalyserException.ExceptionType.WRONG_FILE);
-		} catch (IOException e) {
-			throw new CensusAnalyserException("IO Exception", CensusAnalyserException.ExceptionType.IO_EXCEPTION);
-		} catch (RuntimeException e) {
-			throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.RUNTIME_EXCEPTION);
-		}
+		List<IndiaCensusCSV> listOfEntries = this.getListOfData(csvFilePath, IndiaCensusCSV.class);
+		Comparator<IndiaCensusCSV> comparator = Comparator.comparing(indiaCensus -> indiaCensus.areaInSqKm);
+		List<IndiaCensusCSV> sortedListOfEntries = this.sortList(listOfEntries, comparator);
+		Collections.reverse(sortedListOfEntries);
+		String json = new Gson().toJson(sortedListOfEntries);
+		return json;
 	}
 
 	private <E> List<E> sortList(List<E> listOfEntries, Comparator<E> comparator) {
