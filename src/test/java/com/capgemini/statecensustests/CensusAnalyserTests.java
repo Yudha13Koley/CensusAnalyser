a00d74888a17956aa969dec8888f0d3f32f8e558
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-18 11:49:43 +0530

    Added : Function to sort the state census data by area of state per sq km from highest to lowest

--- src/test/java/com/capgemini/statecensustests/CensusAnalyserTests.java
+++ src/test/java/com/capgemini/statecensustests/CensusAnalyserTests.java
@@ -103,4 +103,17 @@ public class CensusAnalyserTests {
 			fail();
 		}
 	}
+
+	@Test
+	public void givenIndianCensusCSVFile_WhenSortedByAreaOfState_ShouldReturnSortedList() {
+		try {
+			CensusAnalyser censusAnalyser = new CensusAnalyser();
+			String sortedlist = censusAnalyser.getSortedlistByStateArea(INDIA_CENSUS_CSV_FILE_PATH);
+			IndiaCensusCSV[] arr = new Gson().fromJson(sortedlist, IndiaCensusCSV[].class);
+			Assert.assertEquals("Rajasthan", arr[0].state);
+		} catch (CensusAnalyserException e) {
+			System.out.println(e.getMessage());
+			fail();
+		}
+	}
 }
